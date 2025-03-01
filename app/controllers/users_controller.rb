f9966a22c6f1868f65ec5d0e3e66cd6620253b00
karthiknaidu <madalakarthiknaidu3@gmail.com>
2025-03-01 16:39:58 +0530

    admin pages working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :authenticate_admin!, only: [ :profile ]
   before_action :authenticate_user!
 
   def index
@@ -6,8 +7,19 @@ class UsersController < ApplicationController
     @users = User.where.not(id: current_user.id)
   end
 
+  def profile
+    @user = User.find(params[:id])
+    @friends = @user.friends
+  end
   def friends
     # Fetch only accepted friends of the current user
     @friends = current_user.accepted_friends
+  end
+  private
+
+  def authenticate_admin!
+    unless current_user&.admin?
+      redirect_to home_index_path, alert: "You must be an admin to access this page."
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,13 @@ Rails.application.routes.draw do
   get "home/index"
   get "admin/dashboard"
   devise_for :users
-  
+
   authenticated :user do
-    root 'home#index', as: :authenticated_root
+    root "home#index", as: :authenticated_root
   end
 
 
-  resources :friendships, only: [:index, :create, :destroy] do
+  resources :friendships, only: [ :index, :create, :destroy ] do
     collection do
       get :requests # Route for viewing received friend requests
       delete :unfriend
@@ -20,23 +20,25 @@ Rails.application.routes.draw do
       delete :unfriend
     end
   end
-  
-  
+  resources :users, only: [] do
+    get "profile", on: :member, to: "users#profile", as: "profile"
+  end
+
   delete "friendships/unfriend/:friend_id", to: "friendships#unfriend", as: "unfriend"
 
-  
+
   get "friend_requests", to: "friendships#requests"
-  
-  resources :users, only: [:index]
+
+  resources :users, only: [ :index ]
 
   get "friends", to: "users#friends"
 
-  root 'welcome#index'
-  
+  root "welcome#index"
+
   namespace :admin do
-    get 'dashboard', to: 'admin#dashboard'
+    get "dashboard", to: "admin#dashboard"
   end
-  devise_scope :user do 
-    get '/users/sign_out' => 'devise/sessions#destroy'
-  end 
+  devise_scope :user do
+    get "/users/sign_out" => "devise/sessions#destroy"
+  end
 end
